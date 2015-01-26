1588865e2a81d08691f2dc459ed68b536c92a5a3
vahitserifsaglam1 <serif.saglam119@gmail.com>
2015-01-26 23:58:43 +0200

    ipBlockError
    Fixed

--- app/plugins/default.php
+++ app/plugins/default.php
@@ -4,7 +4,7 @@
    set::variable("_php_os",PHP_OS);
    set::variable("_host", $_SERVER['HTTP_HOST']);
    set::variable("_useragent", $_SERVER['HTTP_USER_AGENT']);
-   set::variable("_ip",ipBlock::getIp());
+   if(class_exists('ipBlock')) set::variable("_ip",ipBlock::getIp());
    set::variable("_frameworkVersion",$frameWorkVersion);
    set::variable("_frameworkPath",dirname("../index.php"));
 
